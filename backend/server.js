59882953c23ba5d46556ceecc46e556bb3a37a75
ptkbandara <kavindypt2001@gmail.com>
2025-12-27 00:24:55 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+
 const periodRoutes = require("./routes/periodRoutes");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -15,6 +17,9 @@ app.use(express.json());
 
 // Routes
 app.use("/api/periods", periodRoutes);
+app.use("/api/auth", authRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 
